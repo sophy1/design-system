399255129547efab0148f15f0d5141e1f6716c3d
sophy1 <jihye.sophy.won@gmail.com>
2023-02-21 11:41:08 +0900

    Update test2.js

--- src/test2.js
+++ src/test2.js
@@ -1,7 +1,30 @@
 import { within, userEvent } from '@storybook/testing-library'
 
-MyStory.play = (context) => {
-  const canvas = within(context.canvasElement)
-  // not awaited!
-  userEvent.click(canvas.getByRole('button'))
-}
+
+const Template = (args) => <LoginForm {...args} />;
+
+/*
+* See https://storybook.js.org/docs/react/writing-stories/play-function#working-with-the-canvas
+* to learn more about using the canvasElement to query the DOM
+*/
+export const FilledForm = Template.bind({});
+FilledForm.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  // 👇 Simulate interactions with the component
+  // await userEvent.type(canvas.getByTestId('email'), 'email@provider.com');
+  // not-await alaram!
+  userEvent.type(canvas.getByTestId('email'), 'email@provider.com');
+  
+  await userEvent.type(canvas.getByTestId('password'), 'a-random-password');
+
+  // See https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args to learn how to setup logging in the Actions panel
+//   await userEvent.click(canvas.getByRole('button'));
+
+//   // 👇 Assert DOM structure
+//   await expect(
+//     canvas.getByText(
+//       'Everything is perfect. Your account is ready and we should probably get you started!'
+//     )
+//   ).toBeInTheDocument();
+};
